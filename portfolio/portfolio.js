533242ca11ce1bb8cd7af82ea5d7a997eba93bb1
Adam Yun <adam@Adams-MacBook-Air.local>
2023-08-30 20:59:32 +0800

    mac pushing to update

--- portfolio/portfolio.js
+++ portfolio/portfolio.js
@@ -1,5 +1,7 @@
 function toggleNav() {
     const navLinks = document.getElementById('navLinks');
+    const eachLink = document.getElementById('link');
     navLinks.classList.toggle('active');
+    eachLink.classList.toggle('active');
   }
   
