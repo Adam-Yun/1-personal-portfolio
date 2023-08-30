642e777778cebbb521eea3ffa33fcfd66aac9e1a
Nael-Yun <nyun@mail.sfsu.edu>
2023-08-30 21:15:33 +0800

    update repo, delete folders personal-portfolio and backup

--- portfolio/backup/scripts.js
+++
@@ -1,8 +0,0 @@
-$(function() {
-    $('#A-Y').hover(function() {
-      $('#Adam-Yun').css('background-color', 'yellow');
-    }, function() {
-      // on mouseout, reset the background colour
-      $('#Adam-Yun').css('background-color', '');
-    });
-  });
