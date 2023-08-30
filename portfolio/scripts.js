9fb0f5d54cb7293fc8cc01b8630f38f2a08db738
Nael-Yun <nyun@mail.sfsu.edu>
2023-08-31 00:32:42 +0800

    test netlify deploy #12, delete the defaulted name files

--- portfolio/scripts.js
+++
@@ -1,7 +0,0 @@
-function toggleNav() {
-    const navLinks = document.getElementById('navLinks');
-    const eachLink = document.getElementById('link');
-    navLinks.classList.toggle('active');
-    eachLink.classList.toggle('active');
-  }
-  
