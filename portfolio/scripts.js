cb08b423efe25d32ab21df18c03410eb3e564d40
Adam Yun <adam@Adams-MacBook-Air.local>
2023-08-31 16:01:04 +0800

    made footer height 10vh and submenu more responsive

--- portfolio/scripts.js
+++ portfolio/scripts.js
@@ -1,7 +1,17 @@
 function toggleNav() {
+  
     const navLinks = document.getElementById('navLinks');
-    const eachLink = document.getElementById('link');
+    const listOfLinks = document.getElementById('navLinks')
+    const links = listOfLinks.getElementsByTagName('li');
+
+    // console.log(links);
+
+    for(let i=0; i<=links.length-1; i++){
+      // console.log(links[i]);
+
+      links[i].classList.toggle('active');
+    }
+
     navLinks.classList.toggle('active');
-    eachLink.classList.toggle('active');
   }
   
