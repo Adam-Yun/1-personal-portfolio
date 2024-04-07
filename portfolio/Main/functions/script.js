0dd141ca1acf175bc29d12e6b3bfdc15deb0b8be
Adam Yun <adam@Adams-MacBook-Air.local>
2024-04-07 16:58:54 +0800

    Change file structure and implementation of index,css, and js file

--- portfolio/Main/functions/script.js
+++ portfolio/Main/functions/script.js
@@ -2,7 +2,7 @@
 document.addEventListener("DOMContentLoaded", function () {
     // Function to include navbar
     function includeNavbar() {
-      fetch("navbar.html")
+      fetch("../templates/navbar.html")
         .then(response => response.text())
         .then(html => {
           document.getElementById("navbarContainer").innerHTML = html;
