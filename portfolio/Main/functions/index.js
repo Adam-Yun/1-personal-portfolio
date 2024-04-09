7a9e2cbc9cdc4dcbd0772b699895291a6b2ed0ed
Adam Yun <adam@Adams-MacBook-Air.local>
2024-04-09 15:35:38 +0800

    Changed the styles, html and css

--- portfolio/Main/functions/index.js
+++ portfolio/Main/functions/index.js
@@ -1,33 +1,33 @@
 // Function to check screen width and set justify-content property
 function adjustJustifyContent() {
-    // Get the screen width
-    const screenWidth = window.innerWidth;
+    // // Get the screen width
+    // const screenWidth = window.innerWidth;
 
-    // Select the .main-option-ctn ul element
-    const ulElement = document.querySelector('.main-option-ctn ul');
-    const mainContentCtnElements = document.querySelectorAll(".main-content-ctn > *")
+    // // Select the .main-option-ctn ul element
+    // const ulElement = document.querySelector('.main-option-ctn ul');
+    // const mainContentCtnElements = document.querySelectorAll(".main-content-ctn > *")
 
-    // If screen width is 999px or less, set justify-content to 'start'
-    if (screenWidth <= 999) {
-        ulElement.style.justifyContent = 'start';
-        ulElement.style.alignItems = 'center';
-        mainContentCtnElements.forEach(item => {
-            item.style.justifyContent = 'center'
-            item.style.margin = '0px 0px 0px 0px'
-        });
-    } else {
-        // Reset justify-content to default value if screen width is greater than 999px
-        ulElement.style.justifyContent = 'center';
-        ulElement.style.alignItems = 'start';
-        mainContentCtnElements.forEach(item => {
-            item.style.justifyContent = 'end'
-            item.style.margin = '0px 100px 0px 0px'
-        });
-    }
+    // // If screen width is 999px or less, set justify-content to 'start'
+    // if (screenWidth <= 999) {
+    //     ulElement.style.justifyContent = 'start';
+    //     ulElement.style.alignItems = 'center';
+    //     mainContentCtnElements.forEach(item => {
+    //         item.style.justifyContent = 'center'
+    //         item.style.margin = '0px 0px 0px 0px'
+    //     });
+    // } else {
+    //     // Reset justify-content to default value if screen width is greater than 999px
+    //     ulElement.style.justifyContent = 'center';
+    //     ulElement.style.alignItems = 'start';
+    //     mainContentCtnElements.forEach(item => {
+    //         item.style.justifyContent = 'end'
+    //         item.style.margin = '0px 100px 0px 0px'
+    //     });
+    // }
 }
 
 // Initial adjustment on page load
-adjustJustifyContent();
+// adjustJustifyContent();
 
 // Add event listener for window resize event
-window.addEventListener('resize', adjustJustifyContent);
+// window.addEventListener('resize', adjustJustifyContent);
