9ab4f35acd2e2dfd6d5b6f51ddcde234f06dc4e9
Adam-Yun <naelyunn@gmail.com>
2024-07-05 15:55:04 +0800

    clean up project js functions and made it dynamic

--- portfolio/Main/scripts/project.js
+++ portfolio/Main/scripts/project.js
@@ -1,88 +1,42 @@
-let chevron_ai_chatbot = true
-document.getElementById('chevron-ai-chatbot').addEventListener('click', function() {
 
-    // console.log('chevron clicked')
-    // const viewport = window.innerWidth || document.documentElement.clientWidth; // Screen size
-    const card = document.getElementById('card-ai-chatbot')     // Project's content container
-    const view = document.getElementById('view-ai-chatbot')     // Project's image container
-    const image = document.getElementById('image-ai-chatbot')   // Project's image
-    const extra_description =  document.getElementById('extra-description-ai-chatbot')   // Project's extra description
+dropDown('chevron-ai-chatbot','card-ai-chatbot','view-ai-chatbot','image-ai-chatbot','extra-description-ai-chatbot');
 
-    chevron_ai_chatbot = projectDropDown(card,view,image,extra_description,chevron_ai_chatbot)
-})
+dropDown('chevron-stock-prediction','card-stock-prediction','view-stock-prediction','image-stock-prediction','extra-description-stock-prediction');
 
-let chevron_stock_prediction = true
-document.getElementById('chevron-stock-prediction').addEventListener('click', function() {
+dropDown('chevron-coin-detection','card-coin-detection','view-coin-detection','image-coin-detection','extra-description-coin-detection');
 
-    // console.log('chevron clicked')
-    // const viewport = window.innerWidth || document.documentElement.clientWidth; // Screen size
-    const card = document.getElementById('card-stock-prediction')   // Project's content container
-    const view = document.getElementById('view-stock-prediction')   // Project's image container
-    const image = document.getElementById('image-stock-prediction') // Project's image
-    const extra_description =  document.getElementById('extra-description-stock-prediction')   // Project's extra description
+dropDown('chevron-singapore-pathfinder','card-singapore-pathfinder','view-singapore-pathfinder','image-singapore-pathfinder','extra-description-singapore-pathfinder');
 
-    chevron_stock_prediction = projectDropDown(card,view,image,extra_description,chevron_stock_prediction)
-})
+dropDown('chevron-university-project','card-university-project','view-university-project','image-university-project','extra-description-university-project');
 
-let chevron_coin_detection = true
-document.getElementById('chevron-coin-detection').addEventListener('click', function() {
+dropDown('chevron-mini-projects','card-mini-projects','view-mini-projects','image-mini-projects','extra-description-mini-projects');
 
-    // console.log('chevron clicked')
-    // const viewport = window.innerWidth || document.documentElement.clientWidth; // Screen size
-    const card = document.getElementById('card-coin-detection')   // Project's content container
-    const view = document.getElementById('view-coin-detection')   // Project's image container
-    const image = document.getElementById('image-coin-detection') // Project's image
-    const extra_description =  document.getElementById('extra-description-coin-detection')   // Project's extra description
+popup_not_available('private-ai-chatbot','popup-ai-chatbot');
 
-    chevron_coin_detection = projectDropDown(card,view,image,extra_description,chevron_coin_detection)
-})
+popup_not_available('private-university-project','popup-university-project');
 
-let chevron_singapore_pathfinder = true
-document.getElementById('chevron-singapore-pathfinder').addEventListener('click', function() {
+popup_not_available('private-mini-projects','popup-mini-projects');
 
-    // console.log('chevron clicked')
-    // const viewport = window.innerWidth || document.documentElement.clientWidth; // Screen size
-    const card = document.getElementById('card-singapore-pathfinder')   // Project's content container
-    const view = document.getElementById('view-singapore-pathfinder')   // Project's image container
-    const image = document.getElementById('image-singapore-pathfinder') // Project's image
-    const extra_description =  document.getElementById('extra-description-singapore-pathfinder')   // Project's extra description
+// Event listerner for when the chevron button is clicked
+function dropDown(chevron_name,card_name,view_name,image_name,extra_description_name){
+    let click = true
+    document.getElementById(chevron_name).addEventListener('click', function() {
+        // const viewport = window.innerWidth || document.documentElement.clientWidth; // Screen size
+        const card = document.getElementById(card_name)   // Project's content container
+        const view = document.getElementById(view_name)   // Project's image container
+        const image = document.getElementById(image_name) // Project's image
+        const extra_description =  document.getElementById(extra_description_name)   // Project's extra description
 
-    chevron_singapore_pathfinder = projectDropDown(card,view,image,extra_description,chevron_singapore_pathfinder)
-})
+        click = projectDropDown(click,card,view,image,extra_description)
+    })
+}
 
-let chevron_mini_projects = true
-document.getElementById('chevron-mini-projects').addEventListener('click', function() {
-
-    // console.log('chevron clicked')
-    // const viewport = window.innerWidth || document.documentElement.clientWidth; // Screen size
-    const card = document.getElementById('card-mini-projects')   // Project's content container
-    const view = document.getElementById('view-mini-projects')   // Project's image container
-    const image = document.getElementById('image-mini-projects') // Project's image
-    const extra_description =  document.getElementById('extra-description-mini-projects')   // Project's extra description
-
-    chevron_mini_projects = projectDropDown(card,view,image,extra_description,chevron_mini_projects)
-})
-
-let chevron_university_project = true
-document.getElementById('chevron-university-project').addEventListener('click', function() {
-
-    // console.log('chevron clicked')
-    // const viewport = window.innerWidth || document.documentElement.clientWidth; // Screen size
-    const card = document.getElementById('card-university-project')   // Project's content container
-    const view = document.getElementById('view-university-project')   // Project's image container
-    const image = document.getElementById('image-university-project') // Project's image
-    const extra_description =  document.getElementById('extra-description-university-project')   // Project's extra description
-
-    chevron_university_project = projectDropDown(card,view,image,extra_description,chevron_university_project)
-})
-
-function projectDropDown(card,view,image,extra_description,chevron){
-
-    if(chevron){
+// Function that handles the drop down views of the project's descriptions and images
+function projectDropDown(click,card,view,image,extra_description){
+    if(click){
         card.style.marginBottom = '0px'
         card.style.borderBottom = '0px'
 
-        // view.style.display = 'flex';
         view.style.minHeight = 'min-content'
         view.style.marginBottom = '25px'
         view.style.borderBottom = "1px solid rgb(90, 90, 90)"
@@ -94,12 +48,11 @@ function projectDropDown(card,view,image,extra_description,chevron){
         extra_description.style.opacity = '1'
         extra_description.style.margin = '10px'
 
-        return chevron = false
+        return click = false
     }else{
         card.style.marginBottom = '25px'
         card.style.borderBottom = '1px solid rgb(90, 90, 90)'
 
-        // view.style.display = 'none';
         view.style.minHeight = '0px'
         view.style.marginBottom = '25px'
         view.style.borderBottom = "0px"
@@ -111,34 +64,27 @@ function projectDropDown(card,view,image,extra_description,chevron){
         extra_description.style.opacity = '0'
         extra_description.style.margin = '0px'
 
-        return chevron = true
+        return click = true
     }
-
 }
 
+// Handles popup notifications when github or website link is not available
+function popup_not_available(private_link_name, popup_id){
+    document.getElementById(private_link_name).addEventListener('click', function(){
+        const popup = document.getElementById(popup_id);
 
-document.getElementById('private-github').addEventListener('click', function(){
-
-    // console.log('Private Github')
-    const popup = document.getElementById('popup');
-
-    popup.style.display = 'flex'
-    popup.style.opacity = '1';
-    // setTimeout(function() {
-    //     // popup.style.display = 'none';
-    //     popup.style.opacity = '0';
-    //   }, 2000); // 5000 milliseconds = 3 seconds
-    // popup.style.display = 'none';
-
-    setTimeout(function() {
-        // Change opacity to 0 to fade out
-        popup.style.opacity = '0';
-      
-        // After 2 seconds (2000 milliseconds), hide the element
+        popup.style.display = 'flex'
+        popup.style.opacity = '1';
+    
         setTimeout(function() {
-          popup.style.display = 'none';
-        }, 2000);
-      
-      }, 2000); // Initial timeout of 5 seconds (5000 milliseconds)
-
-})
+            // Change opacity to 0 to fade out
+            popup.style.opacity = '0';
+          
+            // After 2 seconds (2000 milliseconds), hide the element
+            setTimeout(function() {
+                popup.style.display = 'none';
+            }, 1000);
+          
+          }, 2000); // Initial timeout of 5 seconds (5000 milliseconds)
+    })
+}
