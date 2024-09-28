d19c27dd2d8ff2be8be7dcf6f563302f7a55a92c
Adam-Yun <naelyunn@gmail.com>
2024-09-28 18:59:34 +0800

    Improve timing and fix the order of presenting the work title

--- portfolio/Main/scripts/index.js
+++ portfolio/Main/scripts/index.js
@@ -6,10 +6,6 @@ function changeTitle(){
     let fade = 0;
     let count = 1;
 
-    // workTitle.innerHTML = titles[count]
-    // workTitle.classList.remove('fade-out');
-    // workTitle.classList.add('fade-in');
-
     setInterval(() => {
         // console.log(fade)
         if(count >= titles.length){
@@ -32,39 +28,5 @@ function changeTitle(){
         fade++;
     }, 1800); // 3000 milliseconds = 3 seconds    
 }
+
 changeTitle()
-
-
-
-function changeTitleTemp() {
-    const titles = ['Fullstack Developer', 'Computer Vision', 'AI Developer'];
-    let count = 0;
-    const workTitle = document.getElementById('workTitle');
-
-    setInterval(() => {
-        // Add fade-out class to start fading out
-        workTitle.classList.add('fade-out');
-
-        // After fade-out transition ends, change the title
-        setTimeout(() => {
-            count++;
-            if (count >= titles.length) {
-                count = 0;
-            }
-
-            workTitle.innerText = titles[count];
-
-            // Remove fade-out class and add fade-in class to start fading in
-            workTitle.classList.remove('fade-out');
-            workTitle.classList.add('fade-in');
-
-            // Wait for the fade-in to complete before resetting opacity
-            setTimeout(() => {
-                workTitle.classList.remove('fade-in');
-                workTitle.style.opacity = '1'; // Ensure opacity is set to 1 after fading in
-            }, 500); // Duration of the fade-in effect
-        }, 500); // Duration of the fade-out effect
-    }, 3000); // Total interval duration (2 seconds for fading out + 2 seconds for the next title)
-}
-
-// changeTitle();
