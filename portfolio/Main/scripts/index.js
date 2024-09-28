ca1719061f10ecba97f4d9e477cc20517b88cf70
Adam-Yun <naelyunn@gmail.com>
2024-09-28 18:58:49 +0800

    Improve timing and fix the order of presenting the work title

--- portfolio/Main/scripts/index.js
+++ portfolio/Main/scripts/index.js
@@ -4,19 +4,19 @@ function changeTitle(){
     const workTitle = document.getElementById('workTitle')
 
     let fade = 0;
-    let count = 0;
+    let count = 1;
 
-    workTitle.innerHTML = titles[count]
-    workTitle.classList.remove('fade-out');
-    workTitle.classList.add('fade-in');
+    // workTitle.innerHTML = titles[count]
+    // workTitle.classList.remove('fade-out');
+    // workTitle.classList.add('fade-in');
 
     setInterval(() => {
         // console.log(fade)
-        console.log(count)
-        count++;
         if(count >= titles.length){
             count = 0
         }
+
+        console.log(count)
 
         if(fade % 2 == 0){
             // console.log('Even')
@@ -24,12 +24,13 @@ function changeTitle(){
             workTitle.classList.remove('fade-in');
         }else{
             workTitle.innerHTML = titles[count]
+            count++;
             // console.log('Odd')
             workTitle.classList.remove('fade-out');
             workTitle.classList.add('fade-in');
         }
         fade++;
-    }, 1500); // 3000 milliseconds = 3 seconds    
+    }, 1800); // 3000 milliseconds = 3 seconds    
 }
 changeTitle()
 
