0098db27eb4a13e7a69bc82c9fe9d1b89488f482
Adam-Yun <naelyunn@gmail.com>
2025-03-14 15:30:42 +0900

    HTML Updates

--- portfolio/new_main/my-next-app/src/app/page.tsx
+++ portfolio/new_main/my-next-app/src/app/page.tsx
@@ -4,6 +4,52 @@ export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+      </main>
+      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
+        <a
+          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <p>&copy; 2024 Adam Yun</p>
+        </a>
+        <a
+          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+          href=""
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <p>Github</p>
+        </a>
+        <a
+          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+          href=""
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <p>LinkedIn</p>
+        </a>
+        <a
+          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+          href=""
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <p>Email</p>
+        </a>
+      </footer>
+    </div>
+  );
+}
+
+
+/*
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         <Image
           className="dark:invert"
           src="/next.svg"
@@ -101,3 +147,4 @@ export default function Home() {
     </div>
   );
 }
+*/
