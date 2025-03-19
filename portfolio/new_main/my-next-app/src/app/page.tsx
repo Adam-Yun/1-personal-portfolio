503935ef247e090098b62a5bab47e94381a453fc
Adam-Yun <naelyunn@gmail.com>
2025-03-19 20:45:35 +0900

    Made some small changes to nav bar margins

--- portfolio/new_main/my-next-app/src/app/page.tsx
+++ portfolio/new_main/my-next-app/src/app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
     <div id="divBody">
       
       <nav className="p-4 flex justify-between items-center">
-        <a className="name text-xl flex justify-between items-center font-bold">A.Y.</a>
+        <a className="name ml-8 text-2xl flex justify-center items-center font-bold">AY</a>
         <ol id="navbarList" className="w-full mx-40  py-4 px-4 text-xl flex justify-between items-center">
           <li className="">
             <a>Home</a>
@@ -26,7 +26,7 @@ export default function Home() {
             <a>Contact</a>
           </li>
         </ol>
-        <button className="text-xs flex justify-between items-center">Language Options Coming Soon!</button>
+        <button className="text-xs mr-8 flex justify-between items-center">Language Options Coming Soon!</button>
       </nav>
 
       <main id="main" className="p-4 flex justify-center items-center">
