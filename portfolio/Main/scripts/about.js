e07a8e50c71f387a6e540b3b677a7f0cbe3ea615
Adam-Yun <naelyunn@gmail.com>
2025-04-06 17:50:05 +0800

    File and Folder updates

--- portfolio/Main/scripts/about.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', (event) => {
-    const flipContainer = document.getElementById('flipContainer');
-    const flipCard = document.getElementById('flipCard');
-
-    flipContainer.addEventListener('click', () => {
-        flipCard.classList.toggle('flip');
-    });
-});
