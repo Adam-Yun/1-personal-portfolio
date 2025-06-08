80c8d7a0b8fbd07dacf0323e33727356cafe5b5f
Adam-Yun <naelyunn@gmail.com>
2025-06-08 19:45:54 +0800

    Added more CSS

--- portfolio/Main/app/src/app/components/Projects/projects.tsx
+++ portfolio/Main/app/src/app/components/Projects/projects.tsx
@@ -6,17 +6,40 @@ const Projects = () => {
     <div className="projects-box">
         <h2 id="id-projects" className="projects-anchor">Projects Anchor</h2>
         <h2 className="projects-title">Projects</h2>
+
         <ol className="project-lists">
+
           <li className="project-list">
+            <div className="project-information">
+              <h2 className="project-title">A Project Title</h2>
+              <p className="project-description">Build and maintain critical components used to construct Klaviyo&#39;s frontend, across the whole product. Work closely with cross-functional teams, including developers, designers, and product managers, to implement and advocate for best practices in web accessibility.</p>
+              <ol className="techstack">
+                <li className="stack">Python</li>
+                <li className="stack">Python</li>
+                <li className="stack">Python</li>
+                <li className="stack">Python</li>
+              </ol>
+            </div>
+
             <div className="project-image-box">
               <Image src="/images/camping.png" alt="A project image" width={300} height={300} priority/>
             </div>
+          </li>
+
+          <li className="project-list">
             <div className="project-information">
               <h2 className="project-title">A Project Title</h2>
-              <p className="project-description">Build and maintain critical components used to construct Klaviyo’s frontend, across the whole product. Work closely with cross-functional teams, including developers, designers, and product managers, to implement and advocate for best practices in web accessibility.</p>
+              <p className="project-description">Build and maintain critical components used to construct Klaviyo&#39;s frontend, across the whole product. Work closely with cross-functional teams, including developers, designers, and product managers, to implement and advocate for best practices in web accessibility.</p>
               <ol className="techstack">
                 <li className="stack">Python</li>
+                <li className="stack">Python</li>
+                <li className="stack">Python</li>
+                <li className="stack">Python</li>
               </ol>
+            </div>
+
+            <div className="project-image-box">
+              <Image src="/images/camping.png" alt="A project image" width={300} height={300} priority/>
             </div>
           </li>
 
