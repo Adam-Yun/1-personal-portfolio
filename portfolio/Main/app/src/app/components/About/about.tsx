a130b607ae519ab348d25215c2740aac11898b5f
Adam-Yun <naelyunn@gmail.com>
2025-06-26 22:26:27 +1000

    Change about me layout and added image of me

--- portfolio/Main/app/src/app/components/About/about.tsx
+++ portfolio/Main/app/src/app/components/About/about.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image';
 import "./about.css"
 
 const About = () => {
@@ -5,43 +6,49 @@ const About = () => {
     <div className="about-box">
         <h2 id="id-about" className="about-anchor">About Anchor</h2>
         <h2 className="about-title">About</h2>
-        <div className="about-information">
-          <p className="about-name">
-            Adam Yun
-          </p>
+        <div className="about-details">
+          <div className="about-information">
+            <p className="about-name">
+              Adam Yun
+            </p>
 
-          <p className="about-job">
-            AI Engineer
-          </p>
-          <p className="about-description">
-            I build AI apps, large language model, and object detection models.
-          </p>
+            <p className="about-job">
+              AI Engineer
+            </p>
+            <p className="about-description">
+              I build AI apps, large language model, and object detection models.
+            </p>
 
-          <ol>
-            <li>
-              <a href="mailto:adamyunau@gmail.com">
-                <i className="fa fa-envelope-o" ></i>
-              </a>
-            </li>
+            <ol>
+              <li>
+                <a href="mailto:adamyunau@gmail.com">
+                  <i className="fa fa-envelope-o" ></i>
+                </a>
+              </li>
 
-            <li>
-              <a href="https://www.linkedin.com/in/naelyun/" target="_blank">
-                <i className="fa fa-linkedin"></i>
-              </a>
-            </li>
+              <li>
+                <a href="https://www.linkedin.com/in/naelyun/" target="_blank">
+                  <i className="fa fa-linkedin"></i>
+                </a>
+              </li>
 
-            <li>
-              <a href="https://github.com/Adam-Yun" target="_blank">
-                <i className="fa fa-github"></i>
-              </a>
-            </li>
+              <li>
+                <a href="https://github.com/Adam-Yun" target="_blank">
+                  <i className="fa fa-github"></i>
+                </a>
+              </li>
 
-            <li>
-              <a href="" target="">
-                <i className="fa fa-instagram"></i>
-              </a>
-            </li>
-          </ol>
+              <li>
+                <a href="" target="">
+                  <i className="fa fa-instagram"></i>
+                </a>
+              </li>
+            </ol>
+          </div>
+
+          <div className="about-image-box">
+            <Image className="about-image" src="/images/me.JPG" alt="Image of Adam Yun" width={300} height={300} priority/>
+          </div>
         </div>
     </div>
   );
