8523169eea71eea3e60a000a5b0082d338070b21
Adam-Yun <naelyunn@gmail.com>
2025-06-29 19:01:55 +1000

    Fixed link for Adam Yun title

--- portfolio/Main/app/src/app/Navigation/navbar.tsx
+++ portfolio/Main/app/src/app/Navigation/navbar.tsx
@@ -3,7 +3,7 @@ import "./navbar.css"
 const Navbar = () => {
   return (
     <div className="nav-box">
-        <a className="nav-title" href="http://localhost:3000/">Adam Yun</a>
+        <a className="nav-title" href="https://1-personal-portfolio.vercel.app/">Adam Yun</a>
         <ol className="nav-lists">
             <li><a href="#id-about">About</a></li>
             <li><a href="#id-experience">Experience</a></li>
